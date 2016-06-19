33eb1b615ee7dacbd337cc7a70a329acf3ac8b7f
brauliobernardo <=>
2016-06-19 23:26:24 +0200

    Added a footer to all pages. Deleted some inline CSS(ugly practice) and a useless script file. Lots of comments added as well...

--- dist/js/script.js
+++
@@ -1,9 +0,0 @@
-function initMap() {
-  var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-    center: {
-      /*lat: -26.1845227, lng: 28.0994536*/
-      lat: -26.184586, lng: 28.134558
-    }, zoom: 16
-  });
-}
